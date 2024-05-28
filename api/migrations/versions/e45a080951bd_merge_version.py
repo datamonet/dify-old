bf971244aa130ace2cde99f26985cd43ee24a9b0
faye <faye_1225@163.com>
2024-05-28 16:45:54 +0800

    fix:merge db file

---
+++ api/migrations/versions/e45a080951bd_merge_version.py
@@ -0,0 +1,22 @@
+"""Merge version
+
+Revision ID: e45a080951bd
+Revises: 03f98355ba0e, 3e112fce2b3a
+Create Date: 2024-05-28 08:44:33.476185
+
+"""
+import models as models
+
+# revision identifiers, used by Alembic.
+revision = 'e45a080951bd'
+down_revision = ('03f98355ba0e', '3e112fce2b3a')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
