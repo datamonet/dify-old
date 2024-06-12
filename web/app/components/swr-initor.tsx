b9e0839f90a9fdce11c3751d116bbdef2a923772
faye <faye_1225@163.com>
2024-06-12 22:14:38 +0800

    fix:cookie signout bug

--- web/app/components/swr-initor.tsx
+++ web/app/components/swr-initor.tsx
@@ -1,27 +1,25 @@
 'use client'
 
-import { SWRConfig } from 'swr'
-import { useEffect, useState } from 'react'
-import type { ReactNode } from 'react'
+import { useEffect } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 
 type SwrInitorProps = {
-  children: ReactNode
+  // children: ReactNode
   token?: string
 }
 const SwrInitor = ({
-  children,
   token,
 }: SwrInitorProps) => {
   const router = useRouter()
   const searchParams = useSearchParams()
   const consoleToken = searchParams.get('console_token')
   const consoleTokenFromLocalStorage = localStorage?.getItem('console_token')
-  const [init, setInit] = useState(false)
 
   useEffect(() => {
     if (token)
       localStorage?.setItem('console_token', token)
+    else
+      localStorage?.removeItem('console_token')
 
     if (!(consoleToken || consoleTokenFromLocalStorage))
       router.replace('https://takin.ai/auth/signin?callbackUrl=https%3A%2F%2Fdify.takin.ai%2Fapps')
@@ -30,19 +28,9 @@ const SwrInitor = ({
       localStorage?.setItem('console_token', consoleToken!)
       router.replace('/apps', { forceOptimisticNavigation: false } as any)
     }
-    setInit(true)
   }, [])
 
-  return init
-    ? (
-      <SWRConfig value={{
-        shouldRetryOnError: false,
-        revalidateOnFocus: false,
-      }}>
-        {children}
-      </SWRConfig>
-    )
-    : null
+  return null
 }
 
 export default SwrInitor
