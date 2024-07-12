c0a6cd101d1fbe10599d45f5583ec736d450829d
faye <faye_1225@163.com>
2024-07-12 14:54:52 +0800

    add:flask db

---
+++ api/migrations/versions/6c05115583cf_merge_version.py
@@ -0,0 +1,25 @@
+"""merge version
+
+Revision ID: 6c05115583cf
+Revises: 14b6711af67c, 408176b91ad3
+Create Date: 2024-07-12 06:53:35.816535
+
+"""
+from alembic import op
+import models as models
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6c05115583cf'
+down_revision = ('14b6711af67c', '408176b91ad3')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
