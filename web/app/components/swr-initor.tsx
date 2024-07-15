3227a3c62e65bdf8e3a2d9bbab034b627ee67aad
faye <faye_1225@163.com>
2024-07-15 11:07:47 +0800

    test:takin token

--- web/app/components/swr-initor.tsx
+++ web/app/components/swr-initor.tsx
@@ -3,8 +3,7 @@
 import { SWRConfig } from 'swr'
 import { useEffect, useState } from 'react'
 import type { ReactNode } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { getCookie } from '@/app/api/user'
+import { useRouter } from 'next/navigation'
 
 type SwrInitorProps = {
   children: ReactNode
@@ -15,40 +14,22 @@ const SwrInitor = ({
   token,
 }: SwrInitorProps) => {
   const router = useRouter()
-  const searchParams = useSearchParams()
-  const consoleToken = searchParams.get('console_token')
-  const consoleTokenFromLocalStorage = localStorage?.getItem('console_token')
+  // const searchParams = useSearchParams()
+  // const consoleToken = searchParams.get('console_token')
+  // const consoleTokenFromLocalStorage = localStorage?.getItem('console_token')
   const [init, setInit] = useState(false)
-
-  const handleConsoleToken = async () => {
-    await getCookie('__Secure-next-auth.session-token').then((res) => {
-      if (res) {
-        localStorage?.setItem('console_token', res)
-        router.replace('/apps', { forceOptimisticNavigation: false } as any)
-      }
-      else {
-        router.refresh()
-        localStorage?.removeItem('console_token')
-        router.replace('https://takin.ai/auth/signin?callbackUrl=https%3A%2F%2Fdify.takin.ai%2Fapps')
-      }
-    })
-  }
 
   useEffect(() => {
     if (token)
       localStorage?.setItem('console_token', token)
     else
       localStorage?.removeItem('console_token')
-    // takin.ai: 防止重定向速度太快，再查询一次cookie
-    if (!(consoleToken || consoleTokenFromLocalStorage))
-      handleConsoleToken()
 
-    if (consoleToken) {
-      localStorage?.setItem('console_token', consoleToken!)
+    if (localStorage?.getItem('console_token') || token)
       router.replace('/apps', { forceOptimisticNavigation: false } as any)
-    }
+
     setInit(true)
-  }, [])
+  }, [token])
 
   return init
     ? (
