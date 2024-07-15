7d22046546439548fdc7a643849a276becf4d45a
faye <faye_1225@163.com>
2024-07-15 14:21:54 +0800

    test:takin token

--- web/app/components/swr-initor.tsx
+++ web/app/components/swr-initor.tsx
@@ -3,7 +3,7 @@
 import { SWRConfig } from 'swr'
 import { useEffect, useState } from 'react'
 import type { ReactNode } from 'react'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 type SwrInitorProps = {
   children: ReactNode
@@ -14,20 +14,17 @@ const SwrInitor = ({
   token,
 }: SwrInitorProps) => {
   const router = useRouter()
-  // const searchParams = useSearchParams()
-  // const consoleToken = searchParams.get('console_token')
+  const searchParams = useSearchParams()
+  const consoleToken = searchParams.get('console_token')
   // const consoleTokenFromLocalStorage = localStorage?.getItem('console_token')
   const [init, setInit] = useState(false)
 
   useEffect(() => {
-    console.log('进入useEffect hook，token内容：', token)
-    if (token)
-      localStorage?.setItem('console_token', token)
-    else
-      localStorage?.removeItem('console_token')
-
-    if (localStorage?.getItem('console_token') || token)
+    if (token || consoleToken) {
+      localStorage?.setItem('console_token', token || consoleToken as string)
       router.replace('/apps', { forceOptimisticNavigation: false } as any)
+    }
+    else { localStorage?.removeItem('console_token') }
 
     setInit(true)
   }, [])
