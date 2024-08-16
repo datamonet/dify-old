bf6cf68ba6830cefc985843be51c71ebaf645d23
faye <faye_1225@163.com>
2024-08-16 10:06:53 +0800

    merge:new version v0.7

---
+++ api/migrations/versions/2024_08_16_0206-91b1ce192c91_merge_version_v0_7.py
@@ -0,0 +1,25 @@
+"""merge version v0.7
+
+Revision ID: 91b1ce192c91
+Revises: af178a8f4b5a, 8782057ff0dc
+Create Date: 2024-08-16 02:06:32.761265
+
+"""
+from alembic import op
+import models as models
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '91b1ce192c91'
+down_revision = ('af178a8f4b5a', '8782057ff0dc')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
