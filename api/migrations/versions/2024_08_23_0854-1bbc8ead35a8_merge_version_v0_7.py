8a990df6c747efe31df3f6beb81fad72e8cb2415
faye <faye_1225@163.com>
2024-08-23 16:55:01 +0800

    add:db

--- api/migrations/versions/2024_08_23_0854-1bbc8ead35a8_merge_version_v0_7.py
+++ api/migrations/versions/2024_08_23_0854-1bbc8ead35a8_merge_version_v0_7.py
@@ -5,10 +5,7 @@ Revises: 91b1ce192c91, 2dbe42621d96
 Create Date: 2024-08-23 08:54:01.634887
 
 """
-from alembic import op
 import models as models
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision = '1bbc8ead35a8'
