077d9d0e1b776706da9edfe9dcace1c3a5816b8a
faye <faye_1225@163.com>
2024-09-11 16:37:10 +0800

    add:email try catch

--- web/app/api/pricing.ts
+++ web/app/api/pricing.ts
@@ -1,54 +1,9 @@
 'use server'
 import { ObjectId } from 'mongodb'
-import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses'
 import clientPromise from '@/service/mongo'
 import type { NodeTracing } from '@/types/workflow'
 import type { ChatItem } from '@/app/components/base/chat/types'
 import type { ToolItem } from '@/types/app'
-
-export async function emailAWS({
-  address,
-  data,
-  subject,
-}: {
-  address: string[]
-  data: string
-  subject: string
-}) {
-  const client = new SESClient([{
-    region: 'eu-north-1',
-    credentials: {
-      accessKeyId: process.env.EMAIL_ACCESS_KEY_ID,
-      secretAccessKey: process.env.EMAIL_SECRET_ACCESS_KEY,
-    },
-  }])
-
-  const command = new SendEmailCommand({
-    Source: 'noreply@takin.ai',
-    Destination: {
-      ToAddresses: address,
-    },
-    Message: {
-      Body: {
-        Html: {
-          Charset: 'UTF-8',
-          Data: data,
-        },
-      },
-      Subject: {
-        Charset: 'UTF-8',
-        Data: subject,
-      },
-    },
-  })
-  try {
-    return await client.send(command)
-  }
-  catch (e) {
-    console.error(e)
-    return e
-  }
-}
 
 // --------------------------------------------------关于Takin.AI的扣费逻辑解析 start----------------------------------------------------------------------
 /**
