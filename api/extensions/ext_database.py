01304139d9d86796639cd5a3619c53b3d0840fec
faye <faye_1225@163.com>
2024-11-08 16:52:30 +0800

    fix:mongodb

--- api/extensions/ext_database.py
+++ api/extensions/ext_database.py
@@ -7,19 +7,16 @@ from sqlalchemy import MetaData
 
 dotenv.load_dotenv()
 
-
 def create_mongo_client():
     """ 创建并返回 MongoDB 客户端 """
-    client = MongoClient(os.getenv("MONGODB_URI"))
+    client = MongoClient(os.getenv("MONGODB_URI"),connect=False)
     return client
-
 
 def get_users_collection():
     """ 获取用户集合 """
     client = create_mongo_client()  # 每次调用都创建新的 MongoClient 实例
     mongodb = client.get_database(os.getenv("MONGODB_NAME"))
     return mongodb["users"]
-
 
 collection = get_users_collection()
 
